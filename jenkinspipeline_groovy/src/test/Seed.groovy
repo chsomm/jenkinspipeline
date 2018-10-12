84102a2fbe9b5c063b7a0b32036921c1c464c7a7
Chris <Chris@DESKTOP-G87PHD1.fritz.box>
2018-10-12 16:51:00 +0200

    wefwef

--- jenkinspipeline_groovy/src/test/Seed.groovy
+++ jenkinspipeline_groovy/src/test/Seed.groovy
@@ -4,32 +4,32 @@ seedJobs()
 
 def seedJobs() {
 	def parser = new XmlParser()
-	def doc = parser.parse("${workspace}\\jenkinspipeline_groovy\\resources\\jobs.xml");
-	//def doc = parser.parse("..\\jenkinspipeline_groovy\\resources\\jobs.xml");
+	//def doc = parser.parse("${workspace}\\jenkinspipeline_groovy\\resources\\jobs.xml");
+	def doc = parser.parse("..\\jenkinspipeline_groovy\\resources\\jobs.xml");
 
 	println doc
 	
 	for (job in doc) {
 		println "${job['@name']}"
-		println job["workspace"].value
+		println job["workspace"].text()
 	}
 	
-	for (job in doc) {
-		print("Job Name:")
-		println "${job['@name']}"
-
-		print("Job Workspace:")
-		println "${job.workspace[0].text()}"
-
-		println("*******************************")
-		createJob(bk)
-	}
+//	for (job in doc) {
+//		print("Job Name:")
+//		println "${job['@name']}"
+//
+//		print("Job Workspace:")
+//		println "${job.workspace[0].text()}"
+//
+//		println("*******************************")
+//		createJob(job)
+//	}
 }
 
 def createJob(def job) {
 	pipelineJob("${job['@name']}") {
 		parameters {
-			stringParam('BUILD_WORKSPACE', "${job.workspace[0].text()}", 'Build workspace')
+			stringParam('BUILD_WORKSPACE', job["workspace"].text(), 'Build workspace')
 		}
 	}
 }
