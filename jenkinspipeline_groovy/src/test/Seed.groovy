9990ccfbdbe4f03ef9a699897862f9d6b90a4823
Chris <Chris@DESKTOP-G87PHD1.fritz.box>
2018-10-12 15:39:53 +0200

    a

--- jenkinspipeline_groovy/src/test/Seed.groovy
+++ jenkinspipeline_groovy/src/test/Seed.groovy
@@ -1,9 +1,28 @@
 package test;
 
-pipelineJob('example') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('project-a-workflow.groovy'))
-        }
-    }
+def seedJobs() {
+	def parser = new XmlParser()
+	def doc = parser.parse("resources\\jobs.xml");
+
+	doc.job.each{ bk->
+		print("Job Name:")
+		println "${bk['@name']}"
+
+		print("Job Workspace:")
+		println "${bk.workspace[0].text()}"
+
+		println("*******************************")
+		createJob(bk)
+	}
 }
+
+def createJob(def job) {
+	pipelineJob("${bk['@name']}") {
+		definition {
+			cps {
+				script(readFileFromWorkspace('project-a-workflow.groovy'))
+			}
+		}
+	}
+}
+
